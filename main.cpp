832160f812d1f48bc33b52827b95e7e16743054d
Logan <loglim11@gmail.com>
2020-11-22 20:04:49 -0500

    update/refactor

--- main.cpp
+++ main.cpp
@@ -89,18 +89,14 @@ int main(int, char**)
 
     enum Screen {
         SCREEN_HOME,
-        SCREEN_JUPITER,
-        SCREEN_MARS,
-        SCREEN_MOON,
-        SCREEN_EARTH
+        SCREEN_SELECT,
+        SCREEN_LAUNCH
     };
 
-    int state = SCREEN_HOME;
-    //state 0 = home screen
-    //state 1 = jupiter
-    //state 2 = mars
-    //state 3 = moon
-    //state 4 = earth
+    static int state = SCREEN_HOME;
+    static int planet = 0;
+    static int selectedProjectile = 2; // default state is medium projectile; 
+
 
     /*
     ==================== Load Textures ==================== 
@@ -139,6 +135,49 @@ int main(int, char**)
     int rocket_width = 0, rocket_height = 0;
     ret = LoadTextureFromFile("assets/rocket.png", &rocket, &rocket_width, &rocket_height);
     IM_ASSERT(ret);
+
+    // JUPITER SURFACE
+    ID3D11ShaderResourceView* jupiter_surface = NULL;
+    int jupiter_surface_width = 0, jupiter_surface_height = 0;
+    ret = LoadTextureFromFile("assets/jupiter_surface.png", &jupiter_surface, &jupiter_surface_width, &jupiter_surface_height);
+    IM_ASSERT(ret);
+
+    // MARS SURFACE
+    ID3D11ShaderResourceView* mars_surface = NULL;
+    int mars_surface_width = 0, mars_surface_height = 0;
+    ret = LoadTextureFromFile("assets/mars_surface.png", &mars_surface, &mars_surface_width, &mars_surface_height);
+    IM_ASSERT(ret);
+
+    // MOON SURFACE
+    ID3D11ShaderResourceView* moon_surface = NULL;
+    int moon_surface_width = 0, moon_surface_height = 0;
+    ret = LoadTextureFromFile("assets/moon_surface.png", &moon_surface, &moon_surface_width, &moon_surface_height);
+    IM_ASSERT(ret);
+
+    // EARTH SURFACE
+    ID3D11ShaderResourceView* earth_surface = NULL;
+    int earth_surface_width = 0, earth_surface_height = 0;
+    ret = LoadTextureFromFile("assets/earth_surface.png", &earth_surface, &earth_surface_width, &earth_surface_height);
+    IM_ASSERT(ret);
+
+    // CANNON
+    ID3D11ShaderResourceView* cannon = NULL;
+    int cannon_width = 0, cannon_height = 0;
+    ret = LoadTextureFromFile("assets/cannon_45.png", &cannon, &cannon_width, &cannon_height);
+    IM_ASSERT(ret);
+
+    // SQUARE
+    ID3D11ShaderResourceView* square = NULL;
+    int square_width = 0, square_height = 0;
+    ret = LoadTextureFromFile("assets/square.png", &square, &square_width, &square_height);
+    IM_ASSERT(ret);
+
+    // PROJECTILE
+    ID3D11ShaderResourceView* projectile = NULL;
+    int projectile_height = 0, projectile_width = 0;
+    ret = LoadTextureFromFile("assets/projectile2.png", &projectile, &projectile_height, &projectile_width);
+    IM_ASSERT(ret);
+
 
     // Main loop
     MSG msg;
@@ -182,7 +221,8 @@ int main(int, char**)
                 ImGui::SetCursorPos(ImVec2(510, 410));
                 if (ImGui::Button("jupiter", ImVec2(100, 50)))
                 {
-                    state = SCREEN_JUPITER;
+                    state = SCREEN_SELECT;
+                    planet = 3;
                 }
 
                 // Mars TODO add button trigger
@@ -191,7 +231,8 @@ int main(int, char**)
                 ImGui::SetCursorPos(ImVec2(280, 190));
                 if (ImGui::Button("mars", ImVec2(70, 30)))
                 {
-                    state = SCREEN_MARS;
+                    state = SCREEN_SELECT;
+                    planet = 2;
                 }
 
                 // Moon TODO add button trigger
@@ -200,7 +241,8 @@ int main(int, char**)
                 ImGui::SetCursorPos(ImVec2(135, 250));
                 if (ImGui::Button("moon", ImVec2(70, 30)))
                 {
-                    state = SCREEN_MOON;
+                    state = SCREEN_SELECT;
+                    planet = 1;
                 }
 
                 // Earth TODO add button trigger
@@ -209,7 +251,8 @@ int main(int, char**)
                 ImGui::SetCursorPos(ImVec2(55, 420));
                 if (ImGui::Button("earth", ImVec2(80, 40)))
                 {
-                    state = SCREEN_EARTH;
+                    state = SCREEN_SELECT;
+                    planet = 0;
                 }
 
                 // Rocket Graphic
@@ -217,51 +260,41 @@ int main(int, char**)
                 ImGui::Image((void*)rocket, ImVec2(rocket_width * 0.25, rocket_height * 0.25));
             }
 
-            if (true) {
-                int my_image_width = 0;
-                int my_image_height = 0;
-                static int count = 0;
-                ID3D11ShaderResourceView* my_texture = NULL;
-                bool ret = LoadTextureFromFile("background.PNG", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
-                ImGui::SetNextWindowPos(ImVec2(-10, -10));
-                ImGui::SetNextWindowSize(ImVec2(800, 500), 0);
-
-                ImGui::Begin("physics sim", NULL, 1);
-                ImGui::Image((void*)my_texture, ImVec2(800, 500));
-
-                ret = LoadTextureFromFile("Jupiter.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
+            if (state == SCREEN_SELECT) {
+                // place ground
                 ImGui::SetCursorPos(ImVec2(0, 400));
-                ImGui::Image((void*)my_texture, ImVec2(750, my_image_height));
+                switch (planet) {
+                case 0: ImGui::Image((void*)earth_surface, ImVec2(750, earth_surface_height)); break;
+                case 1: ImGui::Image((void*)moon_surface, ImVec2(750, moon_surface_height)); break;
+                case 2: ImGui::Image((void*)mars_surface, ImVec2(750, mars_surface_height)); break;
+                case 3: ImGui::Image((void*)jupiter_surface, ImVec2(750, jupiter_surface_height)); break;
+                }
 
+                // Planet label
                 ImGui::SetCursorPos(ImVec2(350, 440));
                 ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(ImColor(0, 0, 0)));
-
-                ImGui::Text("Jupiter");
+                switch (planet) {
+                case 0: ImGui::Text("Earth"); break;
+                case 1: ImGui::Text("Moon"); break;
+                case 2: ImGui::Text("Mars"); break;
+                case 3: ImGui::Text("Jupiter"); break;
+                }
                 ImGui::PopStyleColor();
 
                 //display cannon
-                ret = LoadTextureFromFile("cannon.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
                 ImGui::SetCursorPos(ImVec2(0, 350));
-                ImGui::Image((void*)my_texture, ImVec2(my_image_width / 2, my_image_height / 2));
+                ImGui::Image((void*)cannon, ImVec2(cannon_width / 2, cannon_height / 2));
 
-                ImGui::SetCursorPos(ImVec2(40, 40));
-                ImGui::Button("Home", ImVec2(75, 30));
-
-                static int selectedProjectile = 0;
+                // Commented for now; no changing environment functionality
+                //ImGui::SetCursorPos(ImVec2(40, 40));
+                //ImGui::Button("Home", ImVec2(75, 30));
 
                 ImGui::SetCursorPos(ImVec2(650, 380));
-                ImGui::Button("Next", ImVec2(75, 30));
+                if (ImGui::Button("Next", ImVec2(75, 30))) {
+                    state = SCREEN_LAUNCH;
+                }
 
-
-                ret = LoadTextureFromFile("square.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
+                // Highlight selected projectile in red
                 if (selectedProjectile == 1)
                     ImGui::SetCursorPos(ImVec2(145, 145));
                 else if (selectedProjectile == 2)
@@ -270,57 +303,49 @@ int main(int, char**)
                     ImGui::SetCursorPos(ImVec2(495, 145));
                 else
                     ImGui::SetCursorPos(ImVec2(1000, 1000));
+                ImGui::Image((void*)square, ImVec2(117, 117));
 
-                ImGui::Image((void*)my_texture, ImVec2(117, 117));
-
-                ret = LoadTextureFromFile("projectile1.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
+                // Begin Large Projectile
                 ImGui::SetCursorPos(ImVec2(500, 150));
-                ImGui::ImageButton(my_texture, ImVec2(100, 100));
+                ImGui::Image(projectile, ImVec2(100, 100));
 
                 ImGui::SetCursorPos(ImVec2(523, 115));
                 if (ImGui::Button("Select##1", ImVec2(60, 20)))
                     selectedProjectile = 3;
 
-
                 ImGui::SetCursorPos(ImVec2(500, 275));
-                ImGui::Text("Properties: \n - ... \n - ...");
+                ImGui::Text("Properties: \n - Radius: 10 \n - Density: 10 \n - Mass: 10");
+                // End Large Projectile
 
-                ret = LoadTextureFromFile("projectile2.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
+                // Begin Medium Projectile
                 ImGui::SetCursorPos(ImVec2(325, 150));
-                ImGui::ImageButton(my_texture, ImVec2(100, 100));
+                ImGui::Image(projectile, ImVec2(100, 100));
 
                 ImGui::SetCursorPos(ImVec2(347, 115));
                 if (ImGui::Button("Select##2", ImVec2(60, 20)))
                     selectedProjectile = 2;
 
                 ImGui::SetCursorPos(ImVec2(325, 275));
-                ImGui::Text("Properties: \n - ... \n - ...");
+                ImGui::Text("Properties: \n - Radius: 5 \n - Density: 5 \n - Mass: 5");
+                // End Medium Projectile
 
-                ret = LoadTextureFromFile("projectile3.png", &my_texture, &my_image_width, &my_image_height);
-                IM_ASSERT(ret);
-
+                // Begin Small Projectile
                 ImGui::SetCursorPos(ImVec2(150, 150));
-                ImGui::ImageButton(my_texture, ImVec2(100, 100));
+                ImGui::Image(projectile, ImVec2(100, 100));
 
                 ImGui::SetCursorPos(ImVec2(173, 115));
                 if (ImGui::Button("Select##3", ImVec2(60, 20)))
                     selectedProjectile = 1;
 
                 ImGui::SetCursorPos(ImVec2(150, 275));
-                ImGui::Text("Properties: \n - ... \n - ...");
+                ImGui::Text("Properties: \n - Radius: 1 \n - Density: 1 \n - Mass: 1");
+                // End Small Projectile
+
+                ImGui::SetCursorPos(ImVec2(400, 50));
+                ImGui::Text("Selected Projectile: %d", selectedProjectile);
             }
-			
-
-			
-			//ImGui::SetCursorPos(ImVec2(400, 50));
-			//ImGui::Text("Selected Projectile: %d", selectedProjectile);
-
+		
             ImGui::End();
-
         }       
 
         // Rendering
